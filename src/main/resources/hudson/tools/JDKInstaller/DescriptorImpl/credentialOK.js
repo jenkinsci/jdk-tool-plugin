a6fdfd4947d5c3cb6b7667f4cf5bfa69fdbb73c7
lemorse <arthurr.caron@gmail.com>
2022-10-24 10:58:49 +0200

    [JENKINS-69860] - onclick usage DescriptorImpl/credentialOK.jelly un-inlined.

---
+++ src/main/resources/hudson/tools/JDKInstaller/DescriptorImpl/credentialOK.js
@@ -0,0 +1,8 @@
+
+document.addEventListener('DOMContentLoaded', (event) => {
+
+    const credentialOkInput = document.getElementById("credentialOkInput");
+
+    credentialOkInput.onclick = (_) => window.close();
+
+});
